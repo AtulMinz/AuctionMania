ba5c5b77d803f305632f2e83e9ecfdf4aebd7ee3
Atulya Minz <atulminz.jsr@gmail.com>
2024-06-15 11:34:27 +0000

    Merged

--- web-ui/src/App.tsx
+++ web-ui/src/App.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import { Button } from './components/ui/button'
+import React from "react";
+import { Button } from "./components/ui/button";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
-    <div><Button>Click</Button></div>
-  )
-}
+    // <><Landing /></>
+    <>
+      <Login />
+    </>
+  );
+};
 
-export default App
+export default App;
