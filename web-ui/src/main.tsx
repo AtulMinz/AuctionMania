937caba877667a8df7baa5824f9cfdc528ff1f5a
Atulya Minz <atulminz.jsr@gmail.com>
2024-06-16 19:47:46 +0000

    Added routes

--- web-ui/src/main.tsx
+++ web-ui/src/main.tsx
@@ -5,6 +5,10 @@ import App from "./App.tsx";
 import "./index.css";
 import Landing from "./pages/Landing.tsx";
 import Login from "./pages/Login.tsx";
+import SelectionPage from "./pages/SelectionPage.tsx";
+import MintPage from "./pages/MintPage.tsx";
+import { Toaster } from "./components/ui/toaster.tsx";
+import AuctionPage from "./pages/AuctionPage.tsx";
 
 const router = createBrowserRouter([
   {
@@ -15,10 +19,23 @@ const router = createBrowserRouter([
     path: "/login",
     element: <Login />,
   },
+  {
+    path: "/services",
+    element: <SelectionPage />,
+  },
+  {
+    path: "/mint",
+    element: <MintPage />,
+  },
+  {
+    path: "/auction",
+    element: <AuctionPage />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Toaster />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
